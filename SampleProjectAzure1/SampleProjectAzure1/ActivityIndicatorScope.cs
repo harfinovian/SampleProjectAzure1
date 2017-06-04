cbf749bb81de2ea514e097ec66bcf58ae9138962
harfinovian <harfinovian@yahoo.com>
2017-06-04 14:34:40 +0700

    firstCommit

---
+++ SampleProjectAzure1/SampleProjectAzure1/ActivityIndicatorScope.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SampleMobileAzure1
+{
+    public class ActivityIndicatorScope : IDisposable
+    {
+        private bool _showIndicator;
+        private ActivityIndicator _indicator;
+        private Task _indicatorDelay;
+
+        public ActivityIndicatorScope(ActivityIndicator indicator, bool showIndicator)
+        {
+            _indicator = indicator;
+            _showIndicator = showIndicator;
+
+            if (showIndicator)
+            {
+                _indicatorDelay = Task.Delay(2000);
+                SetIndicatorActivity(true);
+            }
+            else
+            {
+                _indicatorDelay = Task.FromResult(0);
+            }
+        }
+        private void SetIndicatorActivity(bool isActive)
+        {
+            _indicator.IsVisible = isActive;
+            _indicator.IsRunning = isActive;
+        }
+
+        public void Dispose()
+        {
+            if (_showIndicator)
+            {
+                _indicatorDelay.ContinueWith(t => SetIndicatorActivity(false),
+                    TaskScheduler.FromCurrentSynchronizationContext());
+            }
+        }
+    }
+}
